2f4375018ad20fdbcdc479417de35982162e4a44
Tankery Chen <tankery.chen@gmail.com>
2014-11-13 19:57:34 +0800

    Add A Home table to test Relationed Object persistance.

--- sample.cpp
+++ sample.cpp
@@ -5,6 +5,17 @@
 #include <iostream>
 using namespace std;
 
+class Home {
+	friend class hiberlite::access;
+	template<class Archive>
+	void hibernate(Archive & ar)
+	{
+		ar & HIBERLITE_NVP(key);
+	}
+public:
+    string key;
+};
+
 class Person{
 	friend class hiberlite::access;
 	template<class Archive>
@@ -13,24 +24,34 @@ class Person{
 		ar & HIBERLITE_NVP(name);
 		ar & HIBERLITE_NVP(age);
 		ar & HIBERLITE_NVP(bio);
+		ar & HIBERLITE_NVP(home);
 	}
 public:
 	string name;
 	double age;
 	vector<string> bio;
+    hiberlite::bean_ptr<Home> home;
 };
 
+HIBERLITE_EXPORT_CLASS(Home)
 HIBERLITE_EXPORT_CLASS(Person)
 
 void createDB()
 {
 	hiberlite::Database db("sample.db");
 	//register bean classes
+	db.registerBeanClass<Home>();
 	db.registerBeanClass<Person>();
 	//drop all tables beans will use
 	db.dropModel();
 	//create those tables again with proper schema
 	db.createModel();
+
+    //////////////////
+    // Home 1
+    Home g;
+    g.key = "Home 1";
+	hiberlite::bean_ptr<Home> pg=db.copyBean(g);	//create a managed copy of the object
 
 	const char* names[5]={"Stanley Marsh", "Kyle Broflovski", "Eric Theodore Cartman", "Kenneth McCormick", "Leopold Stotch"};
 
@@ -41,6 +62,24 @@ void createDB()
 		x.bio.push_back("Hello");
 		x.bio.push_back("world");
 		x.bio.push_back("!");
+        x.home = pg;
+
+		hiberlite::bean_ptr<Person> p=db.copyBean(x);	//create a managed copy of the object
+	}
+
+    //////////////////
+    // Home 2
+    g.key = "Home 2";
+	pg=db.copyBean(g);	//create a managed copy of the object
+
+	for(unsigned int i=0;i<5;i++) {
+		Person x;
+		x.name=names[i%5];
+		x.age=14+i*0.1;
+		x.bio.push_back("Hello");
+		x.bio.push_back("world");
+		x.bio.push_back("!");
+        x.home = pg;
 
 		hiberlite::bean_ptr<Person> p=db.copyBean(x);	//create a managed copy of the object
 	}
@@ -49,6 +88,7 @@ void createDB()
 void printDB()
 {
 	hiberlite::Database db("sample.db");
+	db.registerBeanClass<Home>();
 	db.registerBeanClass<Person>();
 
 	cout << string(15,'=')+"\nreading the DB\n";
@@ -58,7 +98,8 @@ void printDB()
 	cout << "found " << v.size() << " persons in the database:\n";
 
 	for(size_t j=0;j<v.size();j++){
-		cout << "[name=" << v[j]->name << "\t";
+		cout << "[home=" << v[j]->home->key << "\t";
+		cout << "name=" << v[j]->name << "\t";
 		cout << "age=" << v[j]->age << "\t";
 		cout << "bio={";
 		for(size_t i=0;i<v[j]->bio.size();i++)
@@ -70,6 +111,7 @@ void printDB()
 void modifyDB()
 {
 	hiberlite::Database db("sample.db");
+	db.registerBeanClass<Home>();
 	db.registerBeanClass<Person>();
 
 	vector< hiberlite::bean_ptr<Person> > v=db.getAllBeans<Person>();
